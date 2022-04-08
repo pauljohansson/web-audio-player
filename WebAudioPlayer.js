f03ef1e76bc73c4bdbd7f9cf8bde409ac9aaa17c
pauljohansson <pralinen@gmail.com>
2022-04-08 13:33:28 +0200

    implement renderTable in View

--- WebAudioPlayer.js
+++ WebAudioPlayer.js
@@ -6,7 +6,31 @@ const playList = new DoublyLinkedList();
 
 // ============== View =========================
 class WebAudioPlayerView {
-  init() {}
+  init() {
+    this.renderTable();
+  }
+
+  renderTable() {
+    const playList = webAudioPlayerApp.getPlayList();
+    const playListTable = document.getElementById("playListTable");
+
+    for (let i = 0; i < playList.length; i++) {
+      const newRow = playListTable.insertRow(i + 1);
+      const trackCell = newRow.insertCell(0);
+      const fileNameCell = newRow.insertCell(1);
+      const durationCell = newRow.insertCell(2);
+
+      trackCell.setAttribute("class", "rightAlignText");
+      durationCell.setAttribute("id", `dur${i + 1}`);
+      durationCell.setAttribute("class", "rightAlignText");
+
+      trackCell.textContent = playList.getElementAtIndex(i).value.trackNumber;
+      fileNameCell.textContent = playList.getElementAtIndex(i).value.fileName;
+      durationCell.textContent = playList.getElementAtIndex(i).value.duration;
+
+      newRow.setAttribute("id", i + 1);
+    }
+  }
 }
 
 //================ Controller ==================
